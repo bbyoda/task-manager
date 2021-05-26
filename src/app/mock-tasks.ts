732a40931af94fe8ac27d6bfd5db1afc48fa4395
Dung~ <h2trungvuong@ocbapngo.org>
2021-05-26 16:52:12 +0200

    first commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import { Task } from './Tasks'
+
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+        
+    },
+    {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'May 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'May 6th at 1:30pm',
+        reminder: true,
+
+    }
+
+
+]
